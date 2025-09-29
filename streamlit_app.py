f22d329ed96aa1d12ea3b5976bb70bee8aa99beb
Laughra Norton <laughran@hotmail.co.uk>
2025-09-29 14:54:47 +0100

    Update streamlit_app.py
    formatting output of REST API response to a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,5 @@ if ingredients_list: # if ingredients_list is not null:
 # new section to display smoothiefroot nutrition info
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
