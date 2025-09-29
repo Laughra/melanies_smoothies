079bf3712f48f8dcb50b094dc8a35dcf46791a19
Laughra Norton <laughran@hotmail.co.uk>
2025-09-29 14:57:46 +0100

    Update streamlit_app.py
    edited code so that the REST API reflects the fruits chosen (apparently)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
@@ -27,6 +28,8 @@ if ingredients_list: # if ingredients_list is not null:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
     #st.write(ingredients_string)
 
@@ -42,8 +45,4 @@ if ingredients_list: # if ingredients_list is not null:
 
         st.success(f'Your Smoothie is ordered, {name_on_order}! ', icon="✅")
 
-# new section to display smoothiefroot nutrition info
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+
